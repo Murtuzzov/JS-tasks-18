04289be18ca04d3f2341062a3f1bce5ab74ce01e
Murtuzzov <131525655+Murtuzzov@users.noreply.github.com>
2024-03-31 23:22:02 +0400

    test

--- js/index.js
+++ js/index.js
@@ -64,3 +64,68 @@
 // console.log(
 //   `Количество чернил в маркере после заправки: ${marker2.markerRefill()}%`
 // );
+
+//___________________________________________________________________________________________________
+
+// Задание 2
+// Реализуйте класс ExtendedDate, унаследовав его от стандартного класса Date и добавив следующие возможности:
+// ■ метод для вывода даты (числа и месяца) текстом;
+// метод для проверки – это прошедшая дата или будущая
+// (если прошедшая, то метод возвращает false; если будущая или текущая, то true);
+// ■ метод для проверки – високосный год или нет;
+// ■ метод, возвращающий следующую дату.
+// Создайте объект класса ExtendedDate и выведите на экран
+// результаты работы новых методов.
+
+// class ExtendedDate extends Date {
+//   constructor(...args) {
+//     super(...args);
+//   }
+
+//   getCurrentDate() {
+//     const months = [
+//       "Январь",
+//       "Февраль",
+//       "Март",
+//       "Апрель",
+//       "Май",
+//       "Июнь",
+//       "Июль",
+//       "Август",
+//       "Сентябрь",
+//       "Октябрь",
+//       "Ноябрь",
+//       "Декабрь",
+//     ];
+
+//     return `Число: ${this.getDate()} | Месяц: ${months[this.getMonth()]}`;
+//   }
+
+//   pastOrFuture(arg) {
+//     if (this.getTime() > arg.getTime()) {
+//       return false;
+//     } else {
+//       return true;
+//     }
+//   }
+
+//   isLeapYear(arg) {
+//     if (arg.getFullYear() % 4 === 0) {
+//       return `${arg.getFullYear()} is leap year`;
+//     } else {
+//       return `${arg.getFullYear()} is non-leap year`;
+//     }
+//   }
+
+// }
+
+// const myDate = new ExtendedDate();
+
+// console.log(myDate.getCurrentDate());
+
+// const newDate = new ExtendedDate("2012-06-22");
+// console.log(myDate.pastOrFuture(newDate));
+
+// const checkLeap = new ExtendedDate("2012-05-04");
+// console.log(myDate.isLeapYear(checkLeap));
+
